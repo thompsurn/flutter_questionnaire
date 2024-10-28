9969cb44d9d5eff745f9057d05003093afa6f87c
thompsurn <121163412+thompsurn@users.noreply.github.com>
2024-10-28 10:11:20 +0000

    Box wrap is now relative on title and thumb icons are better spaced

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
+
   var companyNameDecoration = BoxDecoration(
       border: Border.all(color: Colors.lightBlue, width: 10),
       color: Colors.grey);
 
   var companyNameWidget = Container(
-    margin: EdgeInsets.symmetric(horizontal: 500.0),
-    alignment: Alignment.center,
-    height: 100,
+    padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
     decoration: companyNameDecoration,
-    child: Text(
-          'Company name',
-      style: TextStyle(
-        fontSize: 50,
-        color: Colors.indigo[900],
-      ),
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'Company name here',
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.indigo[900],
+          ),
+        ),
+      ],
     ),
   );
 
-  var questionWidget = Container(
-    child: const Text('Question',
-      style: TextStyle(
-        fontSize: 30,
-      ),
+
+  var questionWidget = const Text('Question here',
+    style: TextStyle(
+      fontSize: 30,
     ),
   );
 
@@ -31,17 +34,19 @@ void main() {
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
       Icon(
-          Icons.thumb_up,
-          color: Colors.green,
+        Icons.thumb_up,
+        color: Colors.green,
         size: 50,
       ),
+      SizedBox(width: 50),
       Icon(
-          Icons.thumb_down,
+        Icons.thumb_down,
         color: Colors.red,
         size: 50,
       ),
     ],
   );
+
 
   runApp(MaterialApp(
     home: Column(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my_first_flutter_project/main.dart';
-
+/*
 void main() {
-  /*
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
@@ -28,5 +25,6 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-   */
+
 }
+*/
